45e946eede8766d1bf4087caae0ebb1dcad1a25b
Suraykina Marina <maarisha333@mail.ru>
2018-12-12 00:44:18 +0300

    Add first file

---
+++ myHomework/src/Homework/lesson2/task2/MainApp.java
@@ -0,0 +1,10 @@
+package Homework.lesson2.task2;
+
+import com.sun.org.apache.xalan.internal.xsltc.compiler.util.MatchGenerator;
+
+public class MainApp {
+    public static void main(String[] args) {
+        double pi= Math.PI;
+        System.out.printf("%.2f",pi);
+    }
+}
